22cc484177195217dada925de9abfe599d20fecc
manibharathi <manibharathi8005@gmail.com>
2025-12-26 12:31:00 +0530

    Switch to bcryptjs and fix initialization order for Vercel

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -7,6 +7,10 @@ if (!cached) {
 }
 
 const connectDB = async () => {
+    if (!process.env.MONGODB_URI) {
+        throw new Error('MONGODB_URI is not defined in environment variables');
+    }
+
     if (cached.conn) {
         return cached.conn;
     }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/mongo');
-require('dotenv').config();
 
-(async () => {
-    await connectDB();
-})();
+// Initial connection
+connectDB().catch(err => {
+    console.error('MDB Connection Error:', err.message);
+});
 
 module.exports = app;
